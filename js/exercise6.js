642f9e316d77f21ab0db03378248790c70e676b1
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2025-03-26 21:55:36 -0500

    feat: :sparkles: in progress

--- js/exercise6.js
+++ js/exercise6.js
@@ -6,10 +6,18 @@ do {
         numeros = numeros.filter(item => typeof item === 'number' && !isNaN(item)) // Filtra solo números válidos dentro del mismo array ya que sin el numeros = numeros crearia otro array 
         let digitos = String(numeros[i]).split('').map(Number);
         let cuadrados = digitos.map(num => num** 2 ); // Elevar cada dígito al cuadrado y map me crea un nuevo array a diferencia de for each
-        if (cuadrados.forEach(num,i,arr) => {})
+        if (numeros[i]==1){
+            console.log("es feliz")
+
+        }
+        
+        console.log(`estos son los digitos que sacamos del for ${digitos}`); 
+        console.log(cuadrados)
+        // if (cuadrados.forEach(num,i,arr) => {})
     
     }
     
+
 } while (resultado.forEach((num,i,arr) =>{
     if (i+1<arr.length && (num+arr[i+1]) ==1 ){
         console.log(`el ${resultado} es un numero feliz`)
@@ -20,7 +28,6 @@ do {
 }));
 resultado.array.length =0; // Borra el contenido del array antes de hacer push
 resultado.push(cuadrados)
-console.log(`estos son los digitos que sacamos del for ${digitos}`); 
 console.log(`el resultado de la multiplicacion es ${resultado}`);
 
 
